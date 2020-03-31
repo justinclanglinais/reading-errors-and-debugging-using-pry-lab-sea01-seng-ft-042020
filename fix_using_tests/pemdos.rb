52e95edfc9fafadb1d5e5093f3ed3a3b17673228
Justin Langlinais <justinclanglinais@gmail.com>
2020-03-31 23:10:18 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,14 +3,8 @@ require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    number_of_s = 1
-    while number_of_s < 10 do 
-      binding.pry
-      before_word = "s"
-      before_word = "#{before_word}" + "s"
-      number_of_s += 1
+    "ssssssssss#{string}"
     end
-    "#{before_word}#{string}"
   else
     string
   end
